a8daed1c80cc9b39cc874220be8b86b09c4b8937
sanjanasenthilkumar15 <sanjanasenthilkumar15@gmail.com>
2026-07-11 19:23:26 +0530

    Fix unused React import error in legal pages

--- src/pages/TermsAndConditions.tsx
+++ src/pages/TermsAndConditions.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { Helmet } from 'react-helmet-async';
 import { useNavigate } from 'react-router-dom';
 import Navbar from '../components/layout/Navbar';
